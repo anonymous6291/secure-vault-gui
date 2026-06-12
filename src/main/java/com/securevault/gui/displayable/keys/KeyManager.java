c2ec8cb60911b8f14f4c899a5013e83728de98f3
anonymous6291 <anishkumarshaw02@gmail.com>
2026-06-12 22:32:11 +0530

    UI upgraded.

--- src/main/java/com/securevault/gui/displayable/keys/KeyManager.java
+++ src/main/java/com/securevault/gui/displayable/keys/KeyManager.java
@@ -11,7 +11,6 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.Semaphore;
@@ -22,9 +21,7 @@ public class KeyManager implements KeyViewListener {
     private final JFrame windowFrame;
     private final KeyManagerListener keyManagerListener;
     private final KeyType keyType;
-    private final Dimension dimension;
     private final JPanel displayPanel;
-    private final JPanel keyViews;
     private final Semaphore lock = new Semaphore(1, true);
     private final Map<Pair, KeyView> keyViewMap = new TreeMap<>();
     private final JPopupMenu addPopupMenu;
@@ -46,12 +43,8 @@ public class KeyManager implements KeyViewListener {
         this.windowFrame = windowFrame;
         this.keyManagerListener = keyManagerListener;
         this.keyType = keyType;
-        this.dimension = dimension;
         this.displayPanel = new ImagePanel(ResourceManager.getResource(KEYS_VIEW_BACKGROUND_IMAGE), dimension.width, dimension.height);
         displayPanel.setLayout(new BorderLayout());
-        keyViews = new JPanel(new BorderLayout());
-        keyViews.setOpaque(false);
-        displayPanel.add(keyViews, BorderLayout.CENTER);
         addPopupMenu = new JPopupMenu();
         addPopupMenu.add(getMenuItem("Add Key", _ -> displayAddKeyDialog()));
         allPopupMenu = new JPopupMenu();
@@ -193,26 +186,22 @@ public class KeyManager implements KeyViewListener {
         setLock();
         keyViewMap.put(pair, new KeyView(pair, this));
         unlock();
+        updateUI();
     }
 
     public void addKeyToView(Pair pair) {
         addKeyToView0(pair);
-        updateUI();
-    }
-
-    public void addKeysToView(List<Pair> values) {
-        values.forEach(this::addKeyToView0);
-        updateUI();
     }
 
     private void deleteKeyFromView0(Pair pair) {
         setLock();
+        keyViewMap.remove(pair);
         unlock();
+        updateUI();
     }
 
     public void deleteKeyFromView(Pair pair) {
         deleteKeyFromView0(pair);
-        updateUI();
     }
 
     private void updateUI() {
@@ -228,18 +217,16 @@ public class KeyManager implements KeyViewListener {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     if (e.getButton() == MouseEvent.BUTTON3) {
-                        IO.println("Enter");
                         addPopupMenu.show(jPanel, e.getX(), e.getY());
-                        IO.println("Enter0");
                     }
                 }
             });
             keyViewMap.values().forEach(x -> jPanel.add(x.getView()));
-            keyViews.removeAll();
-            keyViews.validate();
-            keyViews.add(jScrollPane, BorderLayout.CENTER);
-            keyViews.validate();
-            keyViews.repaint();
+            displayPanel.removeAll();
+            displayPanel.validate();
+            displayPanel.add(jScrollPane, BorderLayout.CENTER);
+            displayPanel.validate();
+            displayPanel.repaint();
         } finally {
             unlock();
         }
@@ -285,10 +272,14 @@ public class KeyManager implements KeyViewListener {
         }
         keyManagerListener.addKey(pair, value, keyType);
         addOrEditKeyDialog.setVisible(false);
-        addOrEditDialogValueFieldLabel.setText("");
+        addOrEditDialogValueField.setText("");
     }
 
     private void deleteConfirmButtonPressed() {
+        Pair pair = new Pair(deleteDialogWebsiteField.getText(), deleteDialogIdField.getText());
+        keyManagerListener.deleteKey(pair, keyType);
+        deleteKeyFromView(pair);
+        deleteKeyDialog.setVisible(false);
     }
 
     private void displayAddKeyDialog() {
